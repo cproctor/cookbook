45859a329e1a411488ccccb8cbd585b97650deda
Chris Proctor <chris.proctor@gmail.com>
2018-12-01 09:49:58 -0500

    Added log level option

--- recipes/management/commands/import_recipes.py
+++ recipes/management/commands/import_recipes.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 import yaml
-from recipes.importers import RecipeImporter
+from recipes.importers import RecipeImporter, get_logger, get_log_level
 from django.core.management import call_command
 from pathlib import Path
 
@@ -11,12 +11,16 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-p', "--recipes_path", default='recipe_sources')
         parser.add_argument('-f', '--force', action='store_true', help="Rebuild the database from recipe_sources")
+        parser.add_argument('-e', '--log_level', default='warning')
 
     def handle(self, *args, **options):
         if options['force']:
             Path(self.database).unlink()
             call_command('migrate')
-        importer = RecipeImporter()
+
+        loglevel = get_log_level(options['log_level'])
+        logger = get_logger(__file__, "recipe_importer.log", loglevel)
+        importer = RecipeImporter(logger)
         for f in Path(options['recipes_path']).glob('*.yaml'):
             with open(f) as inf:
                 importer.import_recipe(yaml.load(inf.read()))
