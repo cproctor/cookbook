fda6bdac4d023b0a256a9e358720098b565543d6
Chris Proctor <chris.proctor@gmail.com>
2019-03-20 20:36:17 -0400

    Added default option value to cook view

--- menus/management/commands/shop.py
+++ menus/management/commands/shop.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("menu", nargs='?', help="Name of menu (fuzzy match)")
         parser.add_argument("-r", "--recipes", nargs="*", help="Names of recipes (fuzzy match)")
-        parser.add_argument("-s", "--servings", type=int, help="Number of people to serve")
+        parser.add_argument("-s", "--servings", type=int, default=1, help="Number of people to serve")
 
     def handle(self, *args, **options):
         if not options['menu'] and not options['recipes']:
@@ -27,7 +27,7 @@ class Command(BaseCommand):
                 recipes = [Recipe.get_by_name(name) for name in options['recipes']]
             except (Recipe.DoesNotExist, Recipe.MultipleObjectsReturned) as e:
                 raise CommandError(e)
-            tempMenu = Menu(name="Menu", servings=options['servings'] or 1)
+            tempMenu = Menu(name="Menu", servings=options['servings'])
             tempMenu.save()
             tempMenu.recipes.set(recipes)
             tempMenu.shopping_list()
